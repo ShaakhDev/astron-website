5ddfd366a6d841a3e9dd0a4a0423ec0ef93dd479
Shakhzod Bobolov <shohbobolov98@gmail.com>
2024-12-26 05:09:57 +0500

    intial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,38 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Moliyaviy Yordamchi",
-	description:
-		"Asosiy moliyaviy savodxonlikdan foydalangan holda pullarini to'g'ri boshqarishni istaganlar uchun qulay va bepul dastur.",
-	openGraph: {
-		type: "website",
-		url: "https://moliyaviy-yordamchi-website.vercel.app/",
-		title: "Moliyaviy Yordamchi",
-		description:
-			"Asosiy moliyaviy savodxonlikdan foydalangan holda pullarini to'g'ri boshqarishni istaganlar uchun qulay va bepul dastur.",
-		images: [
-			{
-				url: "/logo.png",
-				width: 512,
-				height: 512,
-				alt: "moliyaviy yordamchi app logo",
-			},
-		],
-	},
+  title: "Astron - onlayn repetitor",
+  description:
+    "ASTRON -  abiturientlar, o'quvchilar, repetitorlar va o'qituvchilar uchun mo'ljallangan mobil ilova.",
+  openGraph: {
+    type: "website",
+    url: "https://astron-website.vercel.app/",
+    title: "Astron - onlayn repetitor",
+    description:
+      "ASTRON -  abiturientlar, o'quvchilar, repetitorlar va o'qituvchilar uchun mo'ljallangan mobil ilova.",
+    images: [
+      {
+        url: "/logo.png",
+        width: 512,
+        height: 512,
+        alt: "astron app logo",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,37 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center w-full p-24">
-			<div className="lg:w-[512px] lg:h-[512px] w-28 h-28">
-				<Image
-					src={"/logo.png"}
-					alt="moliyaviy yordamchi app logo"
-					width={512}
-					height={512}
-					style={{borderRadius: "10px", width: "100%", height: "100%"}}
-				/>
-			</div>
-			<h1 className="text-2xl text-bold text-center my-10">
-				Moliyaviy Yordamchi
-			</h1>
-			<p className="text-center w-[300px] lg:w-[512px]">
-				Asosiy moliyaviy savodxonlikdan foydalangan holda pullarini
-				to&apos;g&apos;ri boshqarishni istaganlar uchun qulay va bepul dastur.
-			</p>
-			<div className="w-full mt-10">
-				<Link
-					href={
-						"https://play.google.com/store/apps/details?id=com.moliyaviyyordamchi"
-					}
-				>
-					<Image
-						src={"/google-play-badge.png"}
-						alt="google play badge"
-						width={270}
-						height={80}
-						className="mx-auto"
-					/>
-				</Link>
-			</div>
-		</main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center w-full p-24">
+      <div className="lg:w-[512px] lg:h-[512px] w-28 h-28">
+        <Image
+          src={"/logo.png"}
+          alt="Astron - online repetitor app logo"
+          width={512}
+          height={512}
+          style={{ borderRadius: "10px", width: "100%", height: "100%" }}
+        />
+      </div>
+      <h1 className="text-2xl text-bold text-center my-10">
+        Astron - online repetitor
+      </h1>
+      <p className="text-center w-[300px] lg:w-[512px]">
+        ASTRON - abiturientlar, o&apos;quvchilar, repetitorlar va
+        o&apos;qituvchilar uchun mo&apos;ljallangan mobil ilova.
+      </p>
+      <div className="w-full mt-10">
+        <Link
+          href={"https://play.google.com/store/apps/details?id=uz.astrontest"}
+        >
+          <Image
+            src={"/google-play-badge.png"}
+            alt="google play badge"
+            width={270}
+            height={80}
+            className="mx-auto"
+          />
+        </Link>
+      </div>
+    </main>
+  );
 }
